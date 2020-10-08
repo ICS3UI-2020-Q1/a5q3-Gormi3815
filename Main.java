3f17dd61b0b6568992f9a892d6d427c4239b8d21
Gormi3815 <71140812+Gormi3815@users.noreply.github.com>
2020-10-08 11:12:20 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
-/**
+import java.util.Scanner;
+/** this program will tell you the factorial of a number
  *
- * @author 
+ * @author isaac
  */
 public class Main {
 
@@ -10,6 +11,14 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+    //if 5=y 5x4x3x2x1
+     Scanner input = new Scanner(System.in);
+     System.out.println("Please enter an integer to see the factorial");
+     int num = input.nextInt();
+     int sum = 1;
+      for (int count = 0; count < num ; count++){
+         int end = (num-count);
+         sum = sum * end;
+      } System.out.print(sum);
   }
 }
